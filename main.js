d5badd751e1766675f8270d71011667a6cd133c8
shona <weepotty@gmail.com>
2023-04-05 14:30:17 +0800

    adds orbit control

--- main.js
+++ main.js
@@ -42,3 +42,5 @@ const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
