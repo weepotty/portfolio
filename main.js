493fd5248663b7bb79888dbd4f76acc86b192f65
shona <weepotty@gmail.com>
2023-04-05 14:29:39 +0800

    adds new torus to scene

--- main.js
+++ main.js
@@ -23,3 +23,11 @@ camera.position.setZ(30);
 // not sure if this is required
 renderer.render(scene, camera);
 
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xff6347,
+});
+
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
