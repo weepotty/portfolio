85f2b7569b99a135a467f29e9e6adcc0cb687a91
shona <weepotty@gmail.com>
2023-04-05 14:29:10 +0800

    sets size of renderer and camera position

--- main.js
+++ main.js
@@ -16,3 +16,6 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById("bg"),
 });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
