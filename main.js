2b579ae80d6306115abd0f7753718b603d049330
shona <weepotty@gmail.com>
2023-05-01 08:31:19 +0800

    adds scroll animations

--- main.js
+++ main.js
@@ -128,3 +128,22 @@ function animate() {
 }
 
 animate();
+
+// scroll animation
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenBottom = document.querySelectorAll(".hidden-bottom");
+
+hiddenElements.forEach((el) => observer.observe(el));
+hiddenBottom.forEach((el) => observer.observe(el));
